7f7c490768a0bb281a9e6f8e02ba124ac4bdf58e
Binesh Sah <binesh.sah@gmail.com>
2017-04-22 15:40:32 -0500

    Initial Commit

---
+++ src/main/java/com/keylesson/controller/UserController.java
@@ -0,0 +1,66 @@
+package com.keylesson.controller;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.keylesson.manager.UserManager;
+import com.keylesson.model.UserModel;
+
+@ManagedBean
+@Named
+public class UserController {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    String name="Please first login";
+
+	@Inject
+	public UserManager userManager;
+	private UserModel model = new UserModel();
+
+	public void setUserManager(UserManager userManager) {
+		this.userManager = userManager;
+	}
+
+	public UserModel getModel() {
+		return model;
+	}
+
+	public void setModel(UserModel model) {
+		this.model = model;
+	}
+
+	/**
+	 * @return
+	 */
+	public String register() {
+		try {
+			if (!model.getPwd().equals(model.getPwdConfirm())) {
+				throw new Exception("Passwords do not match!!");
+			}
+			userManager.addUser(model);
+		} catch (Exception e) {
+			FacesContext.getCurrentInstance().addMessage(
+					null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR,
+							"Registration Failure, " + e.getMessage(), ""));
+			return null;
+		}
+		FacesContext.getCurrentInstance().addMessage(
+				null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO,
+						"Registration Success!", ""));
+		model.reset();
+		return "login";
+	}
+
+}
